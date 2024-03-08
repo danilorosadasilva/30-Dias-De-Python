aba80253851a1ee9df01c7ed7562b4bf2a1254cd
Danilo Rosa da Silva <danilorosadasilva56@gmail.com>
2024-03-07 21:02:58 -0300

    dia 1
    dia 7/3/24

---
+++ Dia_1/OlaMundo.py
@@ -0,0 +1,22 @@
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)             # adiçao(+)
+print(3 - 1)             # subtraçao(-)
+print(2 * 3)             # multiplicaçao(*)
+print(3 / 2)             # divisao(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulos(%)
+print(3 // 2)            # dividir se quebrar(//)
+
+# tipos
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+print(10 - 5)
+print(7 - 2)
